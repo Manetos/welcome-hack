337c9912e08e6eabf3284703f8410f63725e5ae5
Ebby Wiselyn <ebby.wiselyn@scypho.com>
2015-11-10 15:01:54 +0100

    Rewrite API's add audio files

--- nodejs/speech.js
+++ nodejs/speech.js
@@ -4,24 +4,33 @@ var spawn = require('child_process').spawn;
 var Speech = module.exports = function Speech() {
 };
 
-Speech.prototype.sayWelcome = function sayWelcome() {
+Speech.prototype.sayWelcome = function sayWelcome(name) {
     var child = spawn('aplay',
-                      ['-D', 'hw:1', 'hello.wav']);
+                      ['-D', 'hw:1', name + 'welcome.mp3']);
+    child.on('exit', function() {
+        this.sayName(name);
+    }.bind(this));
+};
+
+Speech.prototype._sayName = function _sayName(name) {
+    var child = spawn('aplay',
+                      ['-D', 'hw:1', './audio/' + name + '.mp3']);
     child.on('exit', function() {
         console.log('finish playing welcome');
     });
 };
 
-Speech.prototype.sayGoodbye = function sayGoodbye() {
+Speech.prototype.identifyYourself = function identifyYourself() {
     var child = spawn('vlc',
-                      ['./hello.wav']);
+                      ['./audio/hello.wav']);
     child.on('exit', function() {
         console.log('finish playing welcome');
     });
-
 };
 
 if (!module.parent) {
     var speech = new Speech();
-    speech.sayWelcome();
+    speech.sayWelcome('ebby');
+    speech.sayWelcome('linus');
+    speech.identifyYourself();
 }
