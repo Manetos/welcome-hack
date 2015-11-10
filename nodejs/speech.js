df4ae07ed97423af4c36744610e2f5c71aa90bb3
Ebby Wiselyn <ebby.wiselyn@scypho.com>
2015-11-10 13:42:37 +0100

    Add speech module

--- nodejs/speech.js
+++ nodejs/speech.js
@@ -1,23 +1,27 @@
 'use strict';
-var play = require('play-audio');
+var spawn = require('child_process').spawn;
 
 var Speech = module.exports = function Speech() {
 };
 
 Speech.prototype.sayWelcome = function sayWelcome() {
-    play('welcome.mp3').volume(0.3).controls().loop().on('ended', function() {
-        console.log('End of the song reached');
+    var child = spawn('cvlc',
+                      ['--play-and-exit', './hello.wav']);
+    child.on('exit', function() {
+        console.log('finish playing welcome');
     });
 };
 
 Speech.prototype.sayGoodbye = function sayGoodbye() {
-    play('goodbye.mp3').volume(0.3).controls().loop().on('ended', function() {
-        console.log('End of the song reached');
+    var child = spawn('vlc',
+                      ['./hello.wav']);
+    child.on('exit', function() {
+        console.log('finish playing welcome');
     });
+
 };
 
 if (!module.parent) {
     var speech = new Speech();
     speech.sayWelcome();
-    speech.sayGoodbye();
 }
