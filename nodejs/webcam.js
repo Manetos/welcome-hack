07b8ce8730409aa427a983f6860761002c166120
Ebby Wiselyn <ebby.wiselyn@scypho.com>
2015-11-10 10:56:09 +0100

    Initial commit for webcam

---
+++ nodejs/webcam.js
@@ -0,0 +1,36 @@
+'use strict';
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
+var WebCam = module.exports = function WebCam(device, quality) {
+    this.device = device || '/dev/video0';
+    this.quality = quality || 75;
+};
+
+WebCam.prototype.capture = function capture(filename, cb) {
+    var callback = cb || function() {};
+    var child = spawn('streamer',
+                      ['-c', this.device,
+                       '-b', 16,
+                       '-j', this.quality,
+                       '-o', filename]);
+
+    child.stdout.on('data', function(chunk) {
+        callback(null, chunk);
+    });
+
+    child.on('exit', function() {
+        console.log('exited');
+        callback(null, 'exit');
+    });
+
+    child.on('error', function() {
+        console.log('error');
+        callback(null, 'error');
+    });
+};
+
+if (!module.parent) {
+    var webcam = new WebCam();
+    webcam.capture(new Date().toISOString() + '.jpeg');
+}
